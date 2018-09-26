60ee854aa19be6ca83c4a918513f44df85afa601
hermlon <dev.hermlon@yamx.net>
2018-09-26 23:48:28 +0200

    fixed import for static css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="/css/welcome.css">
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
   </head>
   <body>
     @yield('content')
