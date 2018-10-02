f3fc998a7545673cf830c23c6a13ca5bf057f66e
hermlon <dev.hermlon@yamx.net>
2018-10-02 10:22:01 +0200

    improved layout

--- resources/views/statements/show.blade.php
+++ resources/views/statements/show.blade.php
@@ -1,13 +1,20 @@
 @extends('layout')
 
+@section('title', 'Share link via WhatsApp')
+
 @section('content')
 
   <div class="container">
-    <p class="text-center">Send this link via WhatsApp to see the hidden message:</p>
-    <input type="text" class="form-control" value="{{ $link }}">
-    <a href="whatsapp://send?text={{ $link }}" data-action="share/whatsapp/share"><i class="material-icons">
-chat
-</i> Share via WhatsApp</a>
+    <div class="form-signin">
+      <p class="text-center">Send this link via WhatsApp to see the hidden message:</p>
+      <input type="text" class="form-control btn-margin" value="{{ $link }}">
+      <a href="whatsapp://send?text={{ $link }}" data-action="share/whatsapp/share" class="whatsapp-link">
+        <i class="material-icons whatsapp-icon">chat</i>
+        <span>Share via WhatsApp</span>
+      </a>
+
+      @include('layouts.logo')
+    </div>
   </div>
 
 @endsection
